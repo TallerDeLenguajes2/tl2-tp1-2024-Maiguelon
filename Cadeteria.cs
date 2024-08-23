38a84dd400f6f95ef098894c6ad1ed3f98e6eb5a
Maiguelon <miguel_carretero@outlook.com>
2024-08-23 19:26:27 -0300

    Métodos de pedidos

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -14,5 +14,28 @@ namespace Cadeteria
             cadetes = new List<Cadete>();
             pedidos = new List<Pedido>();
         }
+
+        public void AgregarPedido(Pedido pedido, Cadete cadete)
+        {
+            pedidos.Add(pedido);
+            cadete.AsignarPedido(pedido);
+        }
+
+        public void ReasignarPedido(Pedido pedido, Cadete nuevoCadete)
+        {
+            Cadete cadeteAnterior = pedido.CadeteAsignado;
+            cadeteAnterior.EliminarPedido(pedido);
+            nuevoCadete.AsignarPedido(pedido);
+        }
+
+        public void GenerarInforme()
+        {
+            foreach (var cadete in cadetes)
+            {
+                Console.WriteLine($"Cadete: {cadete.Nombre}, Entregas: {cadete.PedidosEntregados}, Monto Ganado: ${cadete.CalcularMontoGanado()}");
+            }
+            var promedio = cadetes.Average(c => c.PedidosEntregados);
+            Console.WriteLine($"Promedio de envíos por cadete: {promedio}");
+        }
     }
 }
